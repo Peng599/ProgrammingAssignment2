9cf3858ff6efb4d8da135cac82b335010047b8ea
Peng599 <py5963bh@gmail.com>
2015-06-21 08:34:23 -0400

    the intial version of Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## retrun special matrix:
+##   set ==> assign x with input matrix y
+##   get ==> return x
+##   setSolve ==> assign m with solved reverse matrix
+##   getSolve ==> returned cached reverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(Solve) m <<- Solve
+  getSolve <- function() m
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x':
+## first check if the reverse of the matrix esists,
+##   if reverse already cached, return it
+##   if not, solve the reverse, retrun it after cache the reverse
+cacheSolve <- function(x, ...) {  
+  m <- x$getSolve()
+  if(!is.null(m)) {
+    message("getting cached reverse matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setSolve(m)
+  m
 }
